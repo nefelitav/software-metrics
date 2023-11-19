413dab7b51dbbd5ee58dc912215d109899452067
Anandan Krishnsamy <44828456+anandanlk@users.noreply.github.com>
2023-11-19 12:50:20 +0100

    TestProject

--- series-1/TestProject/src/main/java/com/example/App.java
+++ series-1/TestProject/src/main/java/com/example/App.java
@@ -63,4 +63,514 @@ public class App
         assertFalse(False);
         assertFalse(False);
     }
+
+    int sampleFunction()
+    {
+        for (int i=0; i<5; i++)
+        {
+            System.out.println( "Hello World!" );
+            if (i == 3)
+                System.out.println( "i is 3" );
+            else
+                System.out.println( "i is not 3" );
+        }
+        int day = 4;
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        return 5;
+    }
+
+    int sample1Function()
+    {
+        for (int i=0; i<5; i++)
+        {
+            System.out.println( "Hello World!" );
+            if (i == 3)
+                System.out.println( "i is 3" );
+            else
+                System.out.println( "i is not 3" );
+        }
+        int day = 4;
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        for (i=0; i<5; i++)
+        {
+            System.out.println( "Hello World!" );
+            if (i == 3)
+                System.out.println( "i is 3" );
+            else
+                System.out.println( "i is not 3" );
+        }
+        day = 4;
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        return 5;
+    }
+    int sample2Function()
+    {
+        for (int i=0; i<5; i++)
+        {
+            System.out.println( "Hello World!" );
+            if (i == 3)
+                System.out.println( "i is 3" );
+            else
+                System.out.println( "i is not 3" );
+        }
+        int day = 4;
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        for (i=0; i<5; i++)
+        {
+            System.out.println( "Hello World!" );
+            if (i == 3)
+                System.out.println( "i is 3" );
+            else
+                System.out.println( "i is not 3" );
+        }
+        day = 4;
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        for (i=0; i<5; i++)
+        {
+            System.out.println( "Hello World!" );
+            if (i == 3)
+                System.out.println( "i is 3" );
+            else
+                System.out.println( "i is not 3" );
+        }
+        day = 4;
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        for (i=0; i<5; i++)
+        {
+            System.out.println( "Hello World!" );
+            if (i == 3)
+                System.out.println( "i is 3" );
+            else
+                System.out.println( "i is not 3" );
+        }
+        day = 4;
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        for (i=0; i<5; i++)
+        {
+            System.out.println( "Hello World!" );
+            if (i == 3)
+                System.out.println( "i is 3" );
+            else
+                System.out.println( "i is not 3" );
+        }
+        day = 4;
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        return 5;
+    }
 }
